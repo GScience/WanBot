4bdad392cae163dd3747b046f2da64e3ceeeacd9
GScience <GM2000@outlook.com>
2022-08-09 01:49:05 +0800

    使用Vulkan对渲染进行加速

--- WanBot.Graphic.Example/Program.cs
+++ WanBot.Graphic.Example/Program.cs
@@ -1,100 +1,159 @@
 ﻿
+using SharpVk;
 using SkiaSharp;
 using System.Diagnostics;
+using WanBot.Graphic;
 using WanBot.Graphic.UI;
 using WanBot.Graphic.UI.Layout;
 
-var sw = new Stopwatch();
-sw.Start();
-
-var content = new VerticalLayout();
-for (var k = 0; k < 3; ++k)
+void Draw(bool useVk, string savePath = "test.png")
 {
-    var text = new TextBox();
-    text.Text = "Test For Text Rendering";
-    text.Margin = new Margin(10, 10, 10, 10);
-    content.Children.Add(text);
-
-    var horizontalLayout = new HorizontalLayout();
-
-    for (var j = 0; j < 6; ++j)
+    var content = new VerticalLayout();
+    for (var k = 0; k < 3; ++k)
     {
-        var verticalLayout = new VerticalLayout();
-        verticalLayout.Margin = new Margin(5, 5, 5, 5);
+        var text = new TextBox();
+        text.Text = "Test For Text Rendering";
+        text.Margin = new Margin(10, 10, 10, 10);
+        content.Children.Add(text);
 
-        var titleText = new TextBox();
-        titleText.Text = $"Title";
-        titleText.FontPaint.TextSize = 16;
-        titleText.Margin = new Margin(10, 10, 10, 10);
-        verticalLayout.Children.Add(titleText);
+        var horizontalLayout = new HorizontalLayout();
 
-        var rand = new Random();
-        for (var i = 0; i < 6; ++i)
+        for (var j = 0; j < 6; ++j)
         {
-            var image = new ImageBox();
-            var size = (float)rand.NextDouble() * i * 50 + 50;
-            image.Margin = new Margin(5, 5, 5, 5);
-            image.Width = size;
-            image.Height = size;
+            var verticalLayout = new VerticalLayout();
+            verticalLayout.Margin = new Margin(5, 5, 5, 5);
 
-            verticalLayout.Children.Add(image);
+            var titleText = new TextBox();
+            titleText.Text = $"Title";
+            titleText.FontPaint.TextSize = 16;
+            titleText.Margin = new Margin(10, 10, 10, 10);
+            verticalLayout.Children.Add(titleText);
 
-            var imageText = new TextBox();
-            imageText.Text = $"Image({i}.{j})";
-            imageText.FontPaint.TextSize = 16;
-            imageText.Margin = new Margin(10, 10, 10, 10);
-            verticalLayout.Children.Add(imageText);
+            var rand = new Random();
+            for (var i = 0; i < 6; ++i)
+            {
+                var image = new ImageBox();
+                var size = (float)rand.NextDouble() * i * 50 + 50;
+                image.Margin = new Margin(5, 5, 5, 5);
+                image.Width = size;
+                image.Height = size;
+
+                verticalLayout.Children.Add(image);
+
+                var imageText = new TextBox();
+                imageText.Text = $"Image({i}.{j})";
+                imageText.FontPaint.TextSize = 16;
+                imageText.Margin = new Margin(10, 10, 10, 10);
+                verticalLayout.Children.Add(imageText);
+            }
+            horizontalLayout.Children.Add(verticalLayout);
         }
-        horizontalLayout.Children.Add(verticalLayout);
+        content.Children.Add(horizontalLayout);
     }
-    content.Children.Add(horizontalLayout);
+
+    var text1 = new TextBox();
+    text1.Text = "Single Line";
+    text1.Margin = new Margin(10, 10, 10, 10);
+    text1.FontPaint.TextSize = 32;
+    content.Children.Add(text1);
+
+    var text2 = new TextBox();
+    text2.Text = "Single Line 2";
+    text2.Margin = new Margin(10, 10, 10, 10);
+    content.Children.Add(text2);
+
+    for (var i = 0; i < 3; ++i)
+    {
+        var title = new TextBox();
+        title.Text = $"Text Vertical Layout Test {i}";
+        title.Margin = new Margin(10, 10, 10, 10);
+        content.Children.Add(title);
+
+        var image1 = new ImageBox();
+        image1.Width = 150;
+        image1.Height = 150;
+        image1.Margin = new Margin(10, 10, 10, 10);
+
+        var text3 = new TextBox();
+        text3.Text = "Image With Text";
+        text3.FontPaint.TextSize = 32;
+        text3.Height = 150;
+        text3.Margin = new Margin(170, 10, 10, 10);
+        text3.TextVerticalAlignment = (TextVerticalAlignment)i;
+
+        var textWithImage = new Grid();
+        textWithImage.Margin = new Margin(0, 0);
+        textWithImage.Height = 170;
+        textWithImage.Children.Add(image1);
+        textWithImage.Children.Add(text3);
+        content.Children.Add(textWithImage);
+    }
+
+    var rect = content.UpdateLayout(new SKRect(0, 0, 2000, 300));
+
+    var imageInfo = new SKImageInfo((int)rect.Width, (int)rect.Height);
+
+    if (useVk)
+    {
+        using var surface = SKSurface.Create(VkContext.Current.GrContext, false, imageInfo);
+        content.DrawDebug(surface.Canvas);
+
+        if (!string.IsNullOrEmpty(savePath))
+        {
+            using (var output = File.Create(savePath))
+                surface.Snapshot().Encode().SaveTo(output);
+        }
+    }
+    else
+    {
+        using var surface = SKSurface.Create(imageInfo);
+        content.DrawDebug(surface.Canvas);
+
+        if (!string.IsNullOrEmpty(savePath))
+        {
+            using (var output = File.Create(savePath))
+                surface.Snapshot().Encode().SaveTo(output);
+        }
+    }
 }
 
-var text1 = new TextBox();
-text1.Text = "Single Line";
-text1.Margin = new Margin(10, 10, 10, 10);
-text1.FontPaint.TextSize = 32;
-content.Children.Add(text1);
+Draw(true, "vulkan.png");
+Draw(false, "test.png");
 
-var text2 = new TextBox();
-text2.Text = "Single Line 2";
-text2.Margin = new Margin(10, 10, 10, 10);
-content.Children.Add(text2);
-
-for (var i = 0; i < 3; ++i)
+for (var i = 0; i < 10; ++i)
 {
-    var title = new TextBox();
-    title.Text = $"Text Vertical Layout Test {i}";
-    title.Margin = new Margin(10, 10, 10, 10);
-    content.Children.Add(title);
+    Console.WriteLine($"Start {i}");
 
-    var image1 = new ImageBox();
-    image1.Width = 150;
-    image1.Height = 150;
-    image1.Margin = new Margin(10, 10, 10, 10);
+    var sw1 = new Stopwatch();
+    sw1.Start();
 
-    var text3 = new TextBox();
-    text3.Text = "Image With Text";
-    text3.FontPaint.TextSize = 32;
-    text3.Height = 150;
-    text3.Margin = new Margin(170, 10, 10, 10);
-    text3.TextVerticalAlignment = (TextVerticalAlignment)i;
+    for (var j = 0; j < 250; ++j)
+    {
+        Draw(false, "");
+    }
 
-    var textWithImage = new Grid();
-    textWithImage.Margin = new Margin(0, 0);
-    textWithImage.Height = 170;
-    textWithImage.Children.Add(image1);
-    textWithImage.Children.Add(text3);
-    content.Children.Add(textWithImage);
+    sw1.Stop();
+    Console.WriteLine("CPU draw 250 times use {0}s", sw1.Elapsed);
+
+    GC.Collect();
+    GC.WaitForPendingFinalizers();
+
+    if (VkContext.Current.GrContext != null)
+    {
+        Console.WriteLine($"Start {i}");
+
+        var sw2 = new Stopwatch();
+        sw2.Start();
+
+        for (var j = 0; j < 250; ++j)
+        {
+            Draw(true, "");
+        }
+
+        sw2.Stop();
+        Console.WriteLine("GPU draw 250 times use {0}s", sw2.Elapsed);
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
 }
-
-var rect = content.UpdateLayout(new SKRect(0, 0, 2000, 300));
-
-var imageInfo = new SKImageInfo((int)rect.Width, (int)rect.Height);
-var surface = SKSurface.Create(imageInfo);
-content.DrawDebug(surface.Canvas);
-using (var output = File.Create("test.png"))
-    surface.Snapshot().Encode().SaveTo(output);
-
-sw.Stop();
-Console.WriteLine("Use {0}s", sw.Elapsed);
---
+++ WanBot.Graphic/VkContext.cs
@@ -0,0 +1,82 @@
+﻿using SharpVk;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanBot.Graphic
+{
+    public class VkContext : IDisposable
+    {
+        public static VkContext Current = new();
+
+        private Instance instance;
+        public GRContext GrContext { get; }
+
+        internal VkContext()
+        {
+            var grVkBackendContext = new GRSharpVkBackendContext();
+
+            instance = Instance.Create(Array.Empty<string>(), Array.Empty<string>());
+            grVkBackendContext.VkInstance = instance;
+
+            var physicalDevices = instance.EnumeratePhysicalDevices();
+            var physicalDevice = physicalDevices[0];
+            grVkBackendContext.VkPhysicalDevice = physicalDevice;
+
+            var queueFamilyProperties = physicalDevice.GetQueueFamilyProperties();
+
+            var graphicsFamily = queueFamilyProperties
+                .Select((properties, index) => new { properties, index })
+                .SkipWhile(pair => !pair.properties.QueueFlags.HasFlag(QueueFlags.Graphics))
+                .FirstOrDefault()?.index;
+
+            if (graphicsFamily == null)
+                throw new Exception("Unable to find graphics queue");
+
+            var queueInfos = new[]
+            {
+                new DeviceQueueCreateInfo { QueueFamilyIndex = (uint)graphicsFamily.Value, QueuePriorities = new[] { 1f } }
+            };
+
+            var device = physicalDevice.CreateDevice(queueInfos, null, null);
+            grVkBackendContext.VkDevice = device;
+
+            var graphicsQueue = device.GetQueue((uint)graphicsFamily.Value, 0);
+            grVkBackendContext.VkQueue = graphicsQueue;
+            grVkBackendContext.GraphicsQueueIndex = (uint)graphicsFamily.Value;
+
+            grVkBackendContext.GetProcedureAddress = (name, ins, dev) =>
+            {
+                IntPtr ptr;
+                if (dev != null)
+                    ptr = dev.GetProcedureAddress(name);
+                else if (ins != null)
+                    ptr = ins.GetProcedureAddress(name);
+                else
+                    ptr = instance.GetProcedureAddress(name);
+
+                if (ptr == IntPtr.Zero)
+                    throw new Exception("Null ptr");
+                return ptr;
+            };
+
+            GrContext = GRContext.CreateVulkan(grVkBackendContext, null);
+        }
+
+        public void Dispose()
+        {
+            GrContext.Dispose();
+            instance.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
+
+        ~VkContext()
+        {
+            Dispose();
+        }
+    }
+}
