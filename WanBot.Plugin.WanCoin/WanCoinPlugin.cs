b46e8ed016984b76c8559c2bbc3ffd619ab4206d
GScience <GM2000@outlook.com>
2022-08-27 19:39:28 +0800

    创建虚犊币项目

---
+++ WanBot.Plugin.WanCoin/WanCoinPlugin.cs
@@ -0,0 +1,15 @@
+﻿using WanBot.Api;
+
+namespace WanBot.Plugin.WanCoin
+{
+    public class WanCoinPlugin : WanBotPlugin
+    {
+        public override string PluginName => "WanCoin";
+
+        public override string PluginDescription => "虚犊币插件，水的越多币越多~";
+
+        public override string PluginAuthor => "WanNeng";
+
+        public override Version PluginVersion => Version.Parse("1.0.0");
+    }
+}
