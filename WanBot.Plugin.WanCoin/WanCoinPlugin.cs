defc562dc3fc2a2a23f036e10de13224dac9fc68
GScience <GM2000@outlook.com>
2022-10-09 21:31:15 +0800

    加回虚犊币挖矿功能，更改虚犊币消息回复格式

--- WanBot.Plugin.WanCoin/WanCoinPlugin.cs
+++ WanBot.Plugin.WanCoin/WanCoinPlugin.cs
@@ -2,11 +2,15 @@
 using Microsoft.EntityFrameworkCore;
 using System.Numerics;
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using System.Security.Cryptography;
 using System.Text;
 using WanBot.Api;
 using WanBot.Api.Event;
+using WanBot.Api.Message;
 using WanBot.Api.Mirai;
+using WanBot.Api.Mirai.Event;
+using WanBot.Api.Mirai.Message;
 using WanBot.Api.Util;
 using WanBot.Plugin.Essential.EssAttribute;
 using WanBot.Plugin.Essential.Extension;
@@ -19,6 +23,7 @@ namespace WanBot.Plugin.WanCoin
         internal static DateTime StartTime = new DateTime(2022, 10, 1);
 
         internal const long ServerQQId = 1000;
+        internal const long CoinHashMask = 0x99;
 
         public override string PluginName => "WanCoin";
 
@@ -62,7 +67,7 @@ namespace WanBot.Plugin.WanCoin
         public void ImportFrom(string filePath)
         {
             using var wanCoinDb = GetWanCoinDatabase();
-            using var fileReader = File.OpenText(filePath);
+            using var fileReader = System.IO.File.OpenText(filePath);
 
             while (!fileReader.EndOfStream)
             {
@@ -204,13 +209,13 @@ namespace WanBot.Plugin.WanCoin
                 var user = GetWanCoinUser(wanCoinDb, args.Sender.Id);
                 var serverUser = GetWanCoinUser(wanCoinDb, ServerQQId);
                 var sellPrice = GetCurrentPrice(serverUser.CoinCount);
-                long? buyPrice = serverUser.CoinCount == 0 ? 
-                    null : 
+                long? buyPrice = serverUser.CoinCount == 0 ?
+                    null :
                     GetCurrentPrice(serverUser.CoinCount - 1);
 
                 await args.Sender.ReplyAsync(
                     $"您有 {user.CoinCount} 枚虚犊新币\n" +
-                    $"当前币价：卖({sellPrice}) 买({buyPrice?.ToString() ?? "没币了"})");
+                    $"当前币价：卖({sellPrice}) 买({buyPrice?.ToString() ?? "没币了"})", args.GetMessageId());
             }
         }
 
@@ -229,10 +234,10 @@ namespace WanBot.Plugin.WanCoin
             var buyCount = 1;
             try
             {
-                if (!int.TryParse(args.GetNextArgs<string>(), out buyCount) || 
+                if (!int.TryParse(args.GetNextArgs<string>(), out buyCount) ||
                     buyCount <= 0)
                 {
-                    await args.Sender.ReplyAsync($"你要买入的数量好奇怪");
+                    await args.Sender.ReplyAsync($"你要买入的数量好奇怪", args.GetMessageId());
                     return true;
                 }
             }
@@ -244,7 +249,7 @@ namespace WanBot.Plugin.WanCoin
             var serverUser = GetWanCoinUser(wanCoinDb, ServerQQId);
 
             if (serverUser.CoinCount < buyCount)
-                await args.Sender.ReplyAsync($"完犊子了，服务器的币只有{serverUser.CoinCount}了");
+                await args.Sender.ReplyAsync($"完犊子了，服务器的币只有{serverUser.CoinCount}了", args.GetMessageId());
             else
             {
                 // 记录是否购买成功
@@ -277,9 +282,9 @@ namespace WanBot.Plugin.WanCoin
                 }
 
                 if (bought)
-                    await args.Sender.ReplyAsync($"你买了{buyCount}枚虚犊币，总共花了{totalBuyPrice}钱");
+                    await args.Sender.ReplyAsync($"你买了{buyCount}枚虚犊币，总共花了{totalBuyPrice}钱", args.GetMessageId());
                 else
-                    await args.Sender.ReplyAsync($"钱不够了！需要{totalBuyPrice}钱才能买{buyCount}枚币！");
+                    await args.Sender.ReplyAsync($"钱不够了！需要{totalBuyPrice}钱才能买{buyCount}枚币！", args.GetMessageId());
             }
 
             return true;
@@ -300,14 +305,14 @@ namespace WanBot.Plugin.WanCoin
             var sellCount = 1;
             try
             {
-                if (!int.TryParse(args.GetNextArgs<string>(), out sellCount) || 
+                if (!int.TryParse(args.GetNextArgs<string>(), out sellCount) ||
                     sellCount <= 0)
                 {
-                    await args.Sender.ReplyAsync($"你要卖出的数量好奇怪");
+                    await args.Sender.ReplyAsync($"你要卖出的数量好奇怪", args.GetMessageId());
                     return true;
                 }
             }
-            catch (InvalidOperationException) 
+            catch (InvalidOperationException)
             {
             }
 
@@ -336,10 +341,82 @@ namespace WanBot.Plugin.WanCoin
                     serverUser.CoinCount += sellCount;
                     wanCoinDb.SaveChanges();
                 }
-                await args.Sender.ReplyAsync($"你卖了{sellCount}枚虚犊币，赚了{totalSellPrice}钱！");
+                await args.Sender.ReplyAsync($"你卖了{sellCount}枚虚犊币，赚了{totalSellPrice}钱！", args.GetMessageId());
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// 挖矿
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        [MiraiEvent<GroupMessage>(Priority.Lowest)]
+        public async Task OnGroupMessage(MiraiBot bot, GroupMessage e)
+        {
+            // 只获取纯文本消息来计算hash
+            string? msg = null;
+            Source? source = null;
+            foreach (var chain in e.MessageChain)
+            {
+                if (chain is Source s)
+                {
+                    source = s;
+                    continue;
+                }
+                else if (chain is Plain plain)
+                {
+                    if (msg != null)
+                        return;
+
+                    msg = plain.Text;
+                }
+                else
+                    return;
+            }
+            if (msg == null)
+                return;
+
+            // 文本长度必须大于5小于40才属于有效消息
+            if (msg.Length < 5 || msg.Length > 40)
+                return;
+
+            var hash = HashFunc(msg, e.Sender.Group.Id);
+
+            // 是否满足虚犊币
+            if ((hash & CoinHashMask) != CoinHashMask)
+                return;
+
+            using var wanCoinDb = GetWanCoinDatabase();
+
+            // 数据库中是否已经存在
+            if (wanCoinDb.CoinHash.Where((WanCoinHash item) => item.Hash == hash).Any())
+                return;
+
+            // 添加虚犊币
+            lock (this)
+            {
+                // 加币
+                wanCoinDb.CoinHash.Add(
+                    new WanCoinHash
+                    {
+                        GroupId = e.Sender.Group.Id,
+                        Hash = hash,
+                        Str = msg
+                    });
+                var user = GetWanCoinUser(wanCoinDb, e.Sender.Id);
+                ++user.CoinCount;
+                wanCoinDb.SaveChanges();
+            }
+
+            if (source != null)
+                await bot.SendGroupMessageAsync(e.Sender.Group.Id, source.Id, "好，给你一枚虚犊币吧~");
+            else
+                await bot.SendGroupMessageAsync(e.Sender.Group.Id, null, 
+                    $"刚才好像听见有人说：{msg}" +
+                    $"\n感觉这串文字有着特殊的含义，奖励一枚虚犊币");
         }
     }
 }
