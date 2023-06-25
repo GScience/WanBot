662ba37af1fd8757dda9bc1b3b6b058a8d464e5b
GScience <GM2000@outlook.com>
2023-06-25 19:07:37 +0800

    加入动态宝可梦

--- WanBot.Plugin.Pokemon/PokemonPlugin.cs
+++ WanBot.Plugin.Pokemon/PokemonPlugin.cs
@@ -35,6 +35,7 @@ namespace WanBot.Plugin.Pokemon
                 .Command("#来只宝可梦", "随机抽一只宝可梦")
                 .Command("#来只融合宝可梦", "随机抽一只???")
                 .Command("#宝可梦 Id或名称", "查找宝可梦")
+                .Command("#动态宝可梦 Id或名称", "查找宝可梦，并尝试发送动图")
                 .Command("#融合宝可梦", "随机让两只宝可梦合体（？")
                 .Info("融合出噩梦不要怪我x");
 
@@ -60,6 +61,31 @@ namespace WanBot.Plugin.Pokemon
                 var pokemon = _pokeDatabase.Search(searchKey);
                 if (pokemon != null)
                     await SendPokemon(args.Sender, pokemon);
+                else
+                    await args.Sender.ReplyAsync(new MessageBuilder().At(args.Sender).Text($"\n找不到宝可梦 {searchKey} 哦"));
+                return;
+            }
+        }
+
+        [Command("动态宝可梦")]
+        public async Task OnDynamicPokemon(MiraiBot bot, CommandEventArgs args)
+        {
+            if (!args.Sender.HasCommandPermission(this, "Search"))
+                return;
+
+            if (_pokeDatabase == null || _pokeDatabase.Pokemons.Length == 0)
+                return;
+
+            args.Blocked = true;
+
+            // 查找模式
+            var remain = args.GetRemain()?.ToArray();
+            if (remain != null && remain.Length == 1 && remain[0] is Plain plain)
+            {
+                var searchKey = plain.Text;
+                var pokemon = _pokeDatabase.Search(searchKey);
+                if (pokemon != null)
+                    await SendPokemon(args.Sender, pokemon, true);
                 else
                     await args.Sender.ReplyAsync(new MessageBuilder().At(args.Sender).Text($"\n找不到宝可梦 {searchKey} 哦"));
                 return;
@@ -186,8 +212,16 @@ namespace WanBot.Plugin.Pokemon
                 await args.Sender.ReplyAsync($"坏了，{newPokemonName}还没出生");
         }
 
-        private async Task SendPokemon(ISender sender, PokemonElement pokemon)
+        private async Task SendPokemon(ISender sender, PokemonElement pokemon, bool isDynamic = false)
         {
+            if (!int.TryParse(pokemon.Id, out var id) ||
+                id > 649)
+                isDynamic = false;
+
+            var url = isDynamic ?
+                $"https://s1.52poke.wiki/assets/sprite/gen5/{id:D3}.gif" :
+                pokemon.ImageUrl;
+
             var msg = new MessageBuilder()
                 .At(sender)
                 .Text("\n" + pokemon.DisplayName)
