8dc0bd41834252a1024bf666e9633f36ed252c71
GScience <GM2000@outlook.com>
2024-02-13 13:46:14 +0800

    修复#coin命令报错

--- WanBot.Api/Util/CommandDispatcher.cs
+++ WanBot.Api/Util/CommandDispatcher.cs
@@ -24,6 +24,7 @@ namespace WanBot.Api.Util
             try
             {
                 subCmd = commandEvent.GetNextArgs<string>();
+                if (subCmd == null) return false;
             }
             catch (Exception)
             {
